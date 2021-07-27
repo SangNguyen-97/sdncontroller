083557c1e5ff5bdd3ac56512145f1fccc1b82915
Sang Nguyen <phuocsangnguyen97@gmail.com>
2021-07-27 13:15:38 +0200

    first commit

---
+++ src/test/java/de/frankfurtuniversity/AppTest.java
@@ -0,0 +1,20 @@
+package de.frankfurtuniversity;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
