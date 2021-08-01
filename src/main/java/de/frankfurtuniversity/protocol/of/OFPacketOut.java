b5389cfe88a85f54b8b6d102c91b29c891d8c7d3
Sang Nguyen <phuocsangnguyen97@gmail.com>
2021-08-02 00:25:29 +0200

    Added Packet Out

--- src/main/java/de/frankfurtuniversity/protocol/of/OFPacketOut.java
+++ src/main/java/de/frankfurtuniversity/protocol/of/OFPacketOut.java
@@ -1,5 +1,15 @@
 package de.frankfurtuniversity.protocol.of;
 
+import de.frankfurtuniversity.protocol.of.entities.Action;
+
 public class OFPacketOut {
+    public static final int SIZE = OFPacketBase.SIZE + 8; // in bytes
+
+    long buffer_id; // 32 bits
+    int in_port; // 16 bits
+    int actions_len; // 16 bits
+    Action[] actions; // variable
+    byte[] data; // variable
+
     
 }
